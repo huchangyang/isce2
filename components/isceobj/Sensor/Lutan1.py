2ac05f35e5f47ebc3100880f18d230f1d3b79b69
Yohcy <865533709@qq.com>
2025-05-14 09:00:55 +0800

    change default filter method

--- components/isceobj/Sensor/Lutan1.py
+++ components/isceobj/Sensor/Lutan1.py
@@ -71,7 +71,7 @@ ORBIT_DIR = Component.Parameter('_orbitDir',
 
 FILTER_METHOD = Component.Parameter('filterMethod',
                             public_name ='filterMethod',
-                            default = 'combined_weighted_filter',
+                            default = 'filter_orbit',
                             type=str,
                             doc = 'Orbit filter method (poly_filter, physics_filter, combined_weighted_filter, spline_filter)')
 
@@ -104,7 +104,7 @@ class Lutan1(Sensor):
         self._xmlFileList = []
         self.frame = None
         self.doppler_coeff = None
-        self.filterMethod = 'combined_weighted_filter'
+        self.filterMethod = 'filter_orbit'
         self.splineDegree = 5
         self.splineSmoothing = None
         self._tiff = None
