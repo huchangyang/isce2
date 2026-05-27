679315cab96dd50fab3561b179e0737a6162b9d3
Yohcy <865533709@qq.com>
2026-05-27 18:49:29 +0800

    Fix stripmap ionosphere unwrap input naming

--- components/isceobj/StripmapProc/runUnwrapIcu.py
+++ components/isceobj/StripmapProc/runUnwrapIcu.py
@@ -42,6 +42,48 @@ import os
 
 # giangi: taken Piyush code grass.py and adapted
 
+def _replace_ifg_extension(filename, new_extension):
+    if '.flat' in filename:
+        return filename.replace('.flat', new_extension)
+    elif '.int' in filename:
+        return filename.replace('.int', new_extension)
+    else:
+        return filename + new_extension
+
+def _get_iono_look_suffix(self):
+    numberRangeLooksIon = getattr(self, 'numberRangeLooksIon', None)
+    numberAzimuthLooksIon = getattr(self, 'numberAzimuthLooksIon', None)
+
+    if numberRangeLooksIon is None:
+        numberRangeLooksIon = getattr(self.insar, 'numberRangeLooksIon', None)
+    if numberAzimuthLooksIon is None:
+        numberAzimuthLooksIon = getattr(self.insar, 'numberAzimuthLooksIon', None)
+
+    if numberRangeLooksIon is None or numberAzimuthLooksIon is None:
+        return None
+
+    referenceFrame = self._insar.loadProduct(self._insar.referenceSlcCropProduct)
+    azLooks, rgLooks = self.insar.numberOfLooks(referenceFrame, self.posting,
+                                                self.numberAzimuthLooks,
+                                                self.numberRangeLooks)
+    return '_{}rlks_{}alks'.format(rgLooks * numberRangeLooksIon,
+                                   azLooks * numberAzimuthLooksIon)
+
+def _resolve_wrap_name(self, ifgDirname, igramSpectrum):
+    wrapName = os.path.join(ifgDirname, 'filt_' + self.insar.ifgFilename)
+
+    if igramSpectrum in ('low', 'high'):
+        mlSuffix = _get_iono_look_suffix(self)
+        if mlSuffix is not None:
+            mlWrapName = os.path.join(
+                ifgDirname,
+                'filt_' + _replace_ifg_extension(self.insar.ifgFilename,
+                                                 mlSuffix + os.path.splitext(self.insar.ifgFilename)[1]))
+            if os.path.exists(mlWrapName + '.xml'):
+                return mlWrapName
+
+    return wrapName
+
 def runUnwrap(self , igramSpectrum = "full"):
     '''Specific connector from an insarApp object to a Snaphu object.'''
 
@@ -60,27 +102,17 @@ def runUnwrap(self , igramSpectrum = "full"):
             return
         ifgDirname = os.path.join(self.insar.ifgDirname, self.insar.highBandSlcDirname)
 
-    wrapName = os.path.join(ifgDirname , 'filt_' + self.insar.ifgFilename )
+    wrapName = _resolve_wrap_name(self, ifgDirname, igramSpectrum)
 
-    if '.flat' in wrapName:
-        unwrapName = wrapName.replace('.flat', '.unw')
-    elif '.int' in wrapName:
-        unwrapName = wrapName.replace('.int', '.unw')
-    else:
-        unwrapName = wrapName + '.unw'
+    unwrapName = _replace_ifg_extension(wrapName, '.unw')
 
     img1 = isceobj.createImage()
     img1.load(wrapName + '.xml')
     width = img1.getWidth()
 
     # Get amp image name
-    originalWrapName = os.path.join(ifgDirname , self.insar.ifgFilename)
-    if '.flat' in originalWrapName:
-        resampAmpImage = originalWrapName.replace('.flat', '.amp')
-    elif '.int' in originalWrapName:
-        resampAmpImage = originalWrapName.replace('.int', '.amp')
-    else:
-        resampAmpImage = originalWrapName + '.amp'
+    originalWrapName = wrapName.replace('filt_', '', 1)
+    resampAmpImage = _replace_ifg_extension(originalWrapName, '.amp')
 
     ampImage = isceobj.createAmpImage()
     ampImage.setWidth(width)
